12e3bcfd41c92b8bac7901308082b12acdeb50cc
Kasia Muszyńska <47292469+kmuszynska@users.noreply.github.com>
2019-03-08 21:50:38 +0100

    Add files via upload
    zad15

--- angle/src/main/java/angles/Angle.java
+++ angle/src/main/java/angles/Angle.java
@@ -12,18 +12,52 @@ public class Angle {
     private int seconds;
     private String miaraAngle;
 
+    private static final int RIGHT_ANGEL = 90;
+    private static final int STRAIGHT_ANGLE = 180;
+    private static final int FULL_ANGLE = 360;
+    private static final double RADIAN = 1;
+    private static final double DEGREE = 1;
+    private static final double ARCMINUTE = 1;
+    private static final double ARCSECOND = 1;
 
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public int getDegrees() {
+        return degrees;
+    }
+
+    public void setDegrees(int degrees) {
+        this.degrees = degrees;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
 
     public Angle(int degrees, int minutes, int seconds) {
-        if (degrees>=360 || degrees <0){
+        if (degrees >= 360 || degrees < 0) {
             throw new IllegalArgumentException("You should give the number between 0 and 360");
         }
         this.degrees = degrees;
-        if (minutes>60 || minutes<0){
+        if (minutes > 60 || minutes < 0) {
             throw new IllegalArgumentException("You should give the number between 0 and 60");
         }
         this.minutes = minutes;
-        if (seconds>60 || seconds <0){
+        if (seconds > 60 || seconds < 0) {
             throw new IllegalArgumentException("You shoud give the number between 0 and 60");
         }
         this.seconds = seconds;
@@ -34,18 +68,18 @@ public class Angle {
     }
 
     public Angle(int degrees) {
-        if (degrees>=360 || degrees <0){
+        if (degrees >= 360 || degrees < 0) {
             throw new IllegalArgumentException("You should give the number between 0 and 360");
         }
         this.degrees = degrees;
     }
 
     public Angle(int degrees, int minutes) {
-        if (degrees>=360 || degrees <0){
+        if (degrees >= 360 || degrees < 0) {
             throw new IllegalArgumentException("You should give the number between 0 and 360");
         }
         this.degrees = degrees;
-        if (minutes>60 || minutes<0){
+        if (minutes > 60 || minutes < 0) {
             throw new IllegalArgumentException("You should give the number between 0 and 60");
         }
         this.minutes = minutes;
@@ -82,31 +116,31 @@ public class Angle {
         return 1 / (Math.sin(x));
     }
 
-    public void setOfCosine(double x){
-        x=Math.cos(x);
-        double result = Math.acos(x);
+
+    public void setOfCosine(double cosine) {
+        this.setX(Math.acos(cosine));
     }
 
-    public void setOfSine(double x){
-        x=Math.sin(x);
-        double result = Math.asin(x);
+    public void setOfSine(double x) {
+        this.setX(Math.asin(x));
     }
 
-    public void setOfTangent(double x) {
-        x=Math.tan(x);
-        double result = Math.atan(x);
+    public void setOfTangent(double tangent) {
+        this.setX(Math.atan(tangent));
+
     }
-    public void setOfCotangent(double x){
-        x=1/(Math.tan(x));
-        double result = 1/(Math.atan(x));
+
+    public void setOfCotangent(double cotangent) {
+        this.setX(1 / (Math.atan(x)));
     }
-    public void setOfSecans(double x){
-        x=1 / (Math.cos(x));
-        double result = 1/(secans(x));
+
+    public void setOfSecans(double secans) {
+        this.setX(1 / (secans));
+
     }
-    public void setOfCosecans(double x){
-        x=  1 / (Math.sin(x));
-        double result = 1/(cosexans(x));
+
+    public void setOfCosecans(double cosecans) {
+        this.setX(1 / cosecans);
     }
 
     public double radian(double x) {
@@ -120,9 +154,6 @@ public class Angle {
     }
 
 
-    public String toString(int degrees, int minutes, int seconds) {
-        return "Miara kąta: " + degrees + "°" + minutes + "'" + seconds + "\"";
-    }
     @Override
     public String toString() {
         return "Angle{" +
@@ -136,13 +167,147 @@ public class Angle {
         this.seconds = seconds;
     }
 
-    public double setOfPoint(double x0, double y0){ //dwie przyprosokątne znamy, wiec miara kata to bedzie tangens z tych dwoch wartosci
-        return (x0);
+    public double setOfPoint(double x0, double y0) { //dwie przyprosokątne znamy, wiec miara kata to bedzie tangens z tych dwoch wartosci
+        return (x0 / y0);
 
     }
-    public Angle (double x0, double y0){
+
+    public Angle(double x0, double y0) {
         Angle angle = new Angle();
-        angle.setOfPoint(x0,y0);
+        angle.setOfPoint(x0, y0);
+
     }
-    
+
+    public static Angle valueOf(double x) {
+        Angle angle = new Angle();
+        angle.setX(x);
+        return angle;
+    }
+
+    public static Angle valueOf(int degrees, int minutes, int seconds) {
+        Angle angle = new Angle();
+        if (degrees >= 360 || degrees < 0) {
+            throw new IllegalArgumentException("You should give the number between 0 and 360");
+        }
+        angle.degrees = degrees;
+        if (minutes > 60 || minutes < 0) {
+            throw new IllegalArgumentException("You should give the number between 0 and 60");
+        }
+        angle.minutes = minutes;
+        if (seconds > 60 || seconds < 0) {
+            throw new IllegalArgumentException("You shoud give the number between 0 and 60");
+        }
+        angle.seconds = seconds;
+
+        return angle;
+    }
+
+    public static Angle valueOf(int degrees, int minutes) {
+        Angle angle = new Angle();
+        if (degrees >= 360 || degrees < 0) {
+            throw new IllegalArgumentException("You should give the number between 0 and 360");
+        }
+        angle.degrees = degrees;
+        if (minutes > 60 || minutes < 0) {
+            throw new IllegalArgumentException("You should give the number between 0 and 60");
+        }
+        angle.minutes = minutes;
+
+        return angle;
+    }
+
+    public static Angle valueOf(int degrees) {
+        Angle angle = new Angle();
+        if (degrees >= 360 || degrees < 0) {
+            throw new IllegalArgumentException("You should give the number between 0 and 360");
+        }
+        angle.degrees = degrees;
+
+        return angle;
+    }
+
+    public static Angle valueOf(double x0, double y0) {
+        Angle angle = new Angle();
+        angle.setOfPoint(x0, y0);
+        return angle;
+    }
+
+    public Angle add(Angle angle1) {
+        double result = this.getX() + angle1.getX();
+        Angle angle2 = new Angle();
+        angle2.setX(result);
+        return angle2;
+    }
+
+    public Angle sub(Angle angle1) {
+        double result = this.getX() - angle1.getX();
+        Angle angle2 = new Angle();
+        angle2.setX(result);
+        return angle2;
+    }
+
+    public static Angle sum(Angle angle1, Angle angle2) {
+        double result = angle1.getX() + angle2.getX();
+        Angle angleNew = new Angle();
+        angle2.setX(result);
+        return angleNew;
+
+    }
+
+    public static Angle diff(Angle angle1, Angle angle2) {
+        double result = angle1.getX() - angle2.getX();
+        Angle angleNew = new Angle();
+        angleNew.setX(result);
+        return angleNew;
+    }
+
+    public Angle mult(int value) {
+        double result = this.getX() * value;
+        Angle angleNew = new Angle();
+        angleNew.setX(result);
+        return angleNew;
+    }
+
+    public Angle div(int value) {
+        double result = this.getX() / value;
+        Angle angleNew = new Angle();
+        angleNew.setX(result);
+        return angleNew;
+    }
+
+    public Angle prod(Angle angle1, int value) {
+        double result = angle1.getX() * value;
+        Angle angleNew = new Angle();
+        angleNew.setX(result);
+        return angleNew;
+    }
+
+    public Angle quot(Angle angle1, int value) {
+        double result = angle1.getX() / value;
+        Angle angleNew = new Angle();
+        angleNew.setX(result);
+        return angleNew;
+    }
+
+    public Angle compl(Angle angle) {
+        if (angle.degrees < 90) {
+            double result = 90 - angle.degrees;
+            this.setX(result);
+        } else {
+            System.out.println("Kąt nie ma dopełnienia, bo jest równy bądź większy niż 90°");
+        }
+        return this;
+    }
+
+    public Angle suppl(Angle angle){
+        if (angle.degree(x) < 180) {
+            double result = 180 - angle.degree(x);
+            this.setX(result);
+        } else {
+            System.out.println("Kąt nie ma kąta przylegającego, bo jest równy bądź większy niż 180°");
+        }
+        return this;
+    }
+
+
 }
--- angle/src/main/java/angles/MainAngle.java
+++ angle/src/main/java/angles/MainAngle.java
@@ -26,12 +26,36 @@ public class MainAngle {
         result = angle.degree(245);
         System.out.println("Zamiana na stopnie: " + result);
 
-        System.out.println(angle.toString(45,36,17));
-        angle=new Angle(321,25,27);
-        System.out.println("Nowy obiekt kąta z trzema parametrami konstruktora: "+angle);
-        angle=new Angle(325);
-        System.out.println("Obiekt z jednym konstruktorem: "+angle);
-        angle = new Angle(251,32);
-        System.out.println("Obiekt z dwoma konstruktorami: "+angle);
+        System.out.println(angle.toString());
+        angle = new Angle(321, 25, 27);
+        System.out.println("Nowy obiekt kąta z trzema parametrami konstruktora: " + angle.toString());
+        angle = new Angle(325);
+        System.out.println("Obiekt z jednym konstruktorem: " + angle.toString());
+        angle = new Angle(251, 32);
+        System.out.println("Obiekt z dwoma konstruktorami: " + angle.toString());
+
+        angle = new Angle();
+        angle.setOfCosine(50);
+
+        angle.setOfSine(50);
+
+        angle.setOfCotangent(50);
+
+        angle.setOfTangent(50);
+
+        angle.setOfCosecans(50);
+
+        angle.setOfSecans(50);
+
+        angle = new Angle();
+        System.out.println(angle.setOfPoint(2, 4));
+        angle = new Angle();
+
+        System.out.println(angle.add(new Angle(angle.getX())));
+        angle = new Angle();
+        System.out.println(angle.sub(new Angle(angle.getX())));
+        angle = new Angle(25);
+        System.out.println(angle.compl(new Angle()));
+
     }
 }
